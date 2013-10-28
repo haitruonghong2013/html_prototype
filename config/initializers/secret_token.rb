bce85a7ebf15418bcb5be1b82c56cae7167e03b4
HaiTruong <haitruong@cnc.com.vn>
2013-10-28 11:08:13 +0700

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlPrototype::Application.config.secret_token = '2657209157f72ea16b73bac265320adc42ab06388ed34c3f753ad8c9187024f5d48d7284630d6c19ffead8f0896ad3e211c949c177ba41306756cfa88473224b'
